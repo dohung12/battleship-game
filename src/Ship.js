115042fbc4f05cd369c0dac892e80bc83cd0ec8c
dohung <dohuyhung12@gmail.com>
2022-01-02 14:34:12 +0700

    calc where being hit based on length to start point

--- src/Ship.js
+++ src/Ship.js
@@ -1,4 +1,4 @@
-const ship = (startIndex, length) => {
+const Ship = (startIndex, length) => {
   const statusArr = [];
   for (let index = 0; index < length; index += 1) {
     statusArr.push(false);
@@ -7,16 +7,18 @@ const ship = (startIndex, length) => {
 
   const hitStatus = statusArr;
 
-  const hit = (num) => {
-    statusArr[num] = true;
+  const hit = (coord) => {
+    const place = Math.sqrt(
+      (startIndex[0] - coord[0]) ** 2 + (startIndex[1] - coord[1]) ** 2
+    );
+    statusArr[place] = true;
   };
 
   const isSunk = () => {
     sunkStatus = statusArr.every((element) => element === true);
     return sunkStatus;
   };
-  return { startIndex, length, hitStatus, hit, isSunk };
+  return { hitStatus, hit, isSunk };
 };
 
-export default ship;
-module.exports = ship;
+export default Ship;
